71589775d1d9e4ece43c84a7a05e2bc9d32a6807
Dev--xu <xushuangshuang1@gmail.com>
2014-06-30 21:20:42 +0800

    修改了编译的脚本，因为清空脚本会删除掉target目录

--- compile.sh
+++ compile.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+mkdir -p target/classes  target/test-classes
+
 javac -d target/classes src/main/java/com/bodejidi/*.java
 javac -d target/test-classes -classpath target/classes src/main/test/com/bodejidi/*.java
