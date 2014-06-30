e203b6972755c7bec96eddaf1faa131bf1fa5093
Dev--xu <xushuangshuang1@gmail.com>
2014-06-30 21:38:22 +0800

    增加了全局变量并且判断测试是否成功

--- src/main/test/com/bodejidi/FizzBuzzTest.java
+++ src/main/test/com/bodejidi/FizzBuzzTest.java
@@ -5,11 +5,12 @@ public class FizzBuzzTest
 	public static void main(String[] args)
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-
+		boolean judgeResult = true;
 		String actualResult = fizzBuzz.say(1);
 
 		if(false == "1".equals(actualResult))
 		{
+			judgeResult = false;
 			System.out.println("EXCEPTION : "+ "1" + " BUT " + actualResult );
 		}
 		
@@ -17,7 +18,19 @@ public class FizzBuzzTest
 
 		if(false == "2".equals(actualResult))
 		{
+		    	judgeResult = false;
 			System.out.println("EXCEPTION : "+ "2" + " BUT " + actualResult );
 		}
+
+
+		if(judgeResult)
+		{
+			System.out.println("TEST SUCCESS");
+		}
+		else
+		{
+			System.out.println("TEST FAIL");
+		}
+
 	}
 }
