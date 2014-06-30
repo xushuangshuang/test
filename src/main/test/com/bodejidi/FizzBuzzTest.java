dbd19a97db5551a99f1cb78483d00f4b68e25339
Dev--xu <xushuangshuang1@gmail.com>
2014-06-30 21:22:10 +0800

    重构了测试代码的判断

--- src/main/test/com/bodejidi/FizzBuzzTest.java
+++ src/main/test/com/bodejidi/FizzBuzzTest.java
@@ -5,6 +5,19 @@ public class FizzBuzzTest
 	public static void main(String[] args)
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		System.out.println("1".equals(fizzBuzz.say(1)));
+
+		String actualResult = fizzBuzz.say(1);
+
+		if(false == "1".equals(actualResult))
+		{
+			System.out.println("EXCEPTION : "+ "1" + " BUT " + actualResult );
+		}
+		
+		actualResult = fizzBuzz.say(2);
+
+		if(false == "2".equals(actualResult))
+		{
+			System.out.println("EXCEPTION : "+ "2" + " BUT " + actualResult );
+		}
 	}
 }
